cd62cf86fee75e68d3a0a02005746a39548a73a7
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-01 00:59:16 +0100

    Update auth-layout.tsx

--- src/layouts/auth-layout.tsx
+++ src/layouts/auth-layout.tsx
@@ -1,10 +1,16 @@
+import { Header } from '@/components/auth';
+import { Pattern } from '@/components/ui';
+import React from 'react';
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-[100dvh] flex items-center justify-center">
-      <div className="w-full max-w-[400px] px-6 py-8 rounded-xl 
-        bg-background/70 dark:bg-secondary/70 backdrop-blur-sm shadow-sm border border-line">
+    <>
+      <Pattern>
+        <Header />
+        <main className="py-10 mx-auto md:w-[480px] w-[90%]">
         {children}
-      </div>
-    </div>
-  );
+        </main>
+      </Pattern>
+    </>
+  )
 }
