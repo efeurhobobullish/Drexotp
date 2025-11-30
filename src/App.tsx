c16ec525b2ef1feaf2deeaf677d9fcdb72a73045
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-01 00:20:47 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "sonner";
 import { ScrollToTop } from "@/components/ui";
 import { Home } from "@/pages";
 import { Signup, Login } from "@/pages/auth";
-import { Dashboard, Profile, Wallet } from "@/pages/main";
+import { Dashboard, Profile, Wallet, Numbers } from "@/pages/main";
 
 export default function App() {
   return (
@@ -17,6 +17,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/wallet" element={<Wallet />} />
+       <Route path="/numbers" element={<Numbers />} />
       </Routes>
     </>
   );
