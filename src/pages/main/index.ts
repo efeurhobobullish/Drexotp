cb59fb10a19fb467e48779d753ea8f015abead14
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-01 02:17:19 +0100

    Update index.ts

--- src/pages/main/index.ts
+++ src/pages/main/index.ts
@@ -4,3 +4,4 @@ export { default as Profile } from "./profile";
 export { default as Wallet } from "./wallet";
 export { default as Numbers } from "./numbers";
 export { default as Settings } from "./settings";
+export { default as Help } from "./help";
