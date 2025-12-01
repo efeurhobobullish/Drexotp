8e1bd2591200e9ad74437cea379dc7bdda13cecc
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-01 02:00:03 +0100

    Update index.ts

--- src/pages/auth/index.ts
+++ src/pages/auth/index.ts
@@ -1,4 +1,4 @@
 export { default as Signup } from "./signup";
 export { default as Login } from "./login";
-export { default as ForgotPassword } from './forgot-password'
-export { default as ResetPassword } from './reset-password'
+export { default as ForgotPassword } from "./forgot-password";
+export { default as ResetPassword } from "./reset-password",
