ee390ec1ef1f9fe412adc612cd50b4a6750c586e
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-01 02:05:16 +0100

    Update index.ts

--- src/pages/main/index.ts
+++ src/pages/main/index.ts
@@ -1,4 +1,5 @@
 export { default as Dashboard } from "./dashboard";
+export { default as Transactions } from "./transactions";
 export { default as Profile } from "./profile";
 export { default as Wallet } from "./wallet";
 export { default as Numbers } from "./numbers";
